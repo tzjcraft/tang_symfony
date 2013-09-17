b53995f9314c64062b54b8155f048ed36eaca2ba
tzjcraft <tangzj@shinetechchina.com>
2013-09-17 16:28:42 +0800

    controller

--- src/Acme/HelloBundle/Controller/DefaultController.php
+++ src/Acme/HelloBundle/Controller/DefaultController.php
@@ -3,12 +3,14 @@
 namespace Acme\HelloBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
 
     public function indexAction($name, $color, $aaa)
     {
+//        return new Response('Hello ' . $name, 200);
         return $this->render('AcmeHelloBundle:Default:index.html.twig',
                         array('name' => $name, 'color' => $color, 'aaa' => $aaa));
     }
